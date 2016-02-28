ee29c89240d0a21ca865717b4e0d5ebfd26d969a
ssapra2 <ssapra2@illinois.edu>
2016-02-27 18:35:27 -0600

    got a view set up

--- electionsite/urls.py
+++ electionsite/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'electionsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^engine/', include('engine.urls')),
     url(r'^admin/', include(admin.site.urls)),
 ]
